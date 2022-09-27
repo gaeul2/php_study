e91aa68471cc2a602ae7d02505abdab764f413d4
gaeul <e0540@hanmail.net>
2022-09-27 17:59:21 +0900

    9/27일 공부내용

---
+++ form/get1.php
@@ -0,0 +1,5 @@
+<?php
+echo $_GET['id']."님 안녕하세요.";
+/*localhost/form/25.php?id=쓰고싶은값
+으로 접속하면 id값받아서 표시해줌 */
+?>
---
+++ form/get2.php
@@ -0,0 +1,5 @@
+<?php
+echo $_GET["id"].",".$_GET["password"];
+/*localhost/form/2.php?id=쓰고싶은값&password=쓰고싶은값
+으로 접속하면 id값받아서 표시해줌 */
+?>
